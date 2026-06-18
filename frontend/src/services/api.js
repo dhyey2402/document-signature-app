a67c3a41659cc0d1edaf996e8b9534906e2f84ac
dhyey2402 <dhyeyp060@gmail.com>
2026-06-18 20:26:42 +0530

    config(api): point frontend to Render backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://signly-gg2l.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
